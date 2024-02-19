28fc86b7075acd6b68810aa2caa91baa6713ed99
comsoi <40598925+comsoi@users.noreply.github.com>
2024-02-20 00:04:27 +0800

    feat: wezterm right clink copy/paste

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -348,5 +348,29 @@ action = act.ActivateKeyTable {
 {key = "v", mods = "SHIFT|CTRL", action = wezterm.action{PasteFrom = "Clipboard"}},
 {key = "c", mods = "SHIFT|CTRL", action = wezterm.action{CopyTo = "Clipboard"}},
 }
+-- right clink select (not wezterm copy mode),copy, and if don't select anything, paste
+-- https://github.com/wez/wezterm/discussions/3541#discussioncomment-5633570
+config.mouse_bindings = {{
+    event = {
+        Down = {
+            streak = 1,
+            button = "Right"
+        }
+    },
+    mods = "NONE",
+    action = wezterm.action_callback(function(window, pane)
+        local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+        if has_selection then
+            window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+            window:perform_action(act.ClearSelection, pane)
+        else
+            window:perform_action(act({
+                PasteFrom = "Clipboard"
+            }), pane)
+        end
+    end)
+}}
+
+
 
 return config
