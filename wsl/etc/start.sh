2f93afaa073df98c5b63b4c1d9c077e30b8d69b3
comsoi <40598925+comsoi@users.noreply.github.com>
2024-06-01 01:03:24 +0800

    Update aliases for pbcopy and pbpaste commands

--- wsl/etc/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mount --bind /mnt/c/Users/Lenod/ /home/devuan/winhome
-# echo "${PATH//:/$'\n'}" > /dev/tty1
---
+++ wsl/etc/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# mount --bind /mnt/c/Users/Lenod/ /home/devuan/winhome
+# echo "${PATH//:/$'\n'}" > /dev/tty1
+# su wsl -c 'fastfetch' > /dev/tty1 ; exit
