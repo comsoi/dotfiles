97fe0e83d79ebe2cbb50edda92e481a575173e32
comsoi <40598925+comsoi@users.noreply.github.com>
2024-06-01 02:35:03 +0800

    Update aliases for pbcopy and pbpaste commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ set -x PATH $HOME/.local/bin $HOME/bin $PATH
 if status is-interactive
 	# Commands to run in interactive sessions can go here
 	set fish_greeting
-    alias setporxy="proxy_on"
-    alias noproxy="proxy_off"
+	alias setporxy="proxy_on"
+	alias noproxy="proxy_off"
 	proxy_on > /dev/null
 end
