83d02ab16f77c5c7e5a858fe725c966c1829d4ab
comsoi <40598925+comsoi@users.noreply.github.com>
2024-12-12 19:23:53 +0800

    [RFC] enhance zsh configuration; improve keybindings, plugin management, and cursor behavior

--- .config/zsh/00-init.tlp.zsh
+++ .config/zsh/00-init.tlp.zsh
@@ -1,38 +1,57 @@
-setopt AUTO_CD
-setopt INTERACTIVE_COMMENTS
-setopt HIST_FCNTL_LOCK
-setopt HIST_IGNORE_ALL_DUPS
-setopt SHARE_HISTORY
-unsetopt AUTO_REMOVE_SLASH
-unsetopt HIST_EXPIRE_DUPS_FIRST
-unsetopt EXTENDED_HISTORY
-HISTFILE=$ZDOTDIR/.history
+setopt AUTO_CD INTERACTIVE_COMMENTS HIST_FCNTL_LOCK HIST_IGNORE_ALL_DUPS SHARE_HISTORY
+unsetopt AUTO_REMOVE_SLASH HIST_EXPIRE_DUPS_FIRST EXTENDED_HISTORY
+HISTFILE=$ZDOTDIR/.history; KEYTIMEOUT=20
 
 # p10k
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
 	source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
-[[ ! -f ${ZDOTDIR}/.p10k.zsh ]] || source ${ZDOTDIR}/.p10k.zsh
-# Plugins
-if [[ -r /usr/share/zsh/plugins ]]; then
-	source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-	source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-elif [[ -r "${ZDOTDIR}/plugins/custom" ]]; then
-	source ${ZDOTDIR}/plugins/custom/zsh-autosuggestions/zsh-autosuggestions.zsh
-	source ${ZDOTDIR}/plugins/custom/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-else
-	source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-	source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+if [[ $USE_OMZ == true ]]; then
+	unset HISTFILE
+	export ZSH="${ZDOTDIR}/oh-my-zsh"
+	ZSH_THEME="powerlevel10k/powerlevel10k"
+	HYPHEN_INSENSITIVE=# remove esc seq for alt key enter vicmd mod"true"
+	zstyle ':omz:update' mode reminder; zstyle ':omz:update' frequency 26
+	COMPLETION_WAITING_DOTS="true"; DISABLE_UNTRACKED_FILES_DIRTY="true"
+	plugins=(command-not-found extract docker git github gitignore
+	history-substring-search node npm nvm vscode sudo web-search
+	zsh-autosuggestions zsh-syntax-highlighting ohmyzsh-full-autoupdate)
+	source $ZSH/oh-my-zsh.sh
+	return
 fi
-# Theme
-if [[ -r "/usr/share/zsh-theme-powerlevel10k" ]]; then
-	POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
-  POWERLEVEL9K_TERM_SHELL_INTEGRATION=true
-	source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
-elif [[ -r "${ZDOTDIR}/themes/powerlevel10k" ]]; then
-	POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
-	source ${ZDOTDIR}/themes/powerlevel10k/powerlevel10k.zsh-theme
-elif [[ -r "${HOME}/.powerlevel10k" ]]; then
-	POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
-	source ${HOME}/.powerlevel10k/powerlevel10k.zsh-theme
+
+typeset -a PLUGIN_PATHS=("/usr/share/zsh/plugins" "${ZDOTDIR}/plugins/custom")
+typeset -a PLUGINS=("zsh-autosuggestions" "zsh-syntax-highlighting" "zsh-vi-mode")
+typeset PLUGINS_LOADED=false
+for PLUGIN_DIR in "${PLUGIN_PATHS[@]}"; do
+	if [[ -r "$PLUGIN_DIR" ]]; then
+		ZVM_VI_INSERT_ESCAPE_BINDKEY='jj'; ZVM_INIT_MODE='sourcing'
+		for plugin in "${PLUGINS[@]}"; do
+			source "$PLUGIN_DIR/$plugin/$plugin.zsh"
+		done
+		PLUGINS_LOADED=true; break
+	fi
+done
+if ! $PLUGINS_LOADED; then
+	for plugin in "${PLUGINS[@]}"; do
+		source "/usr/share/$plugin/$plugin.zsh"
+	done
+	PLUGINS_LOADED="manual"
 fi
+
+[[ -f ${ZDOTDIR}/.p10k.zsh ]] && source ${ZDOTDIR}/.p10k.zsh
+typeset -a THEME=(
+	"/usr/share/zsh-theme-powerlevel10k"
+	"${ZDOTDIR}/themes/powerlevel10k"
+	# "$(brew --prefix)/share/powerlevel10k"
+)
+# command -v brew >/dev/null 2>&1 && THEME+=("$(brew --prefix)/share/powerlevel10k")
+
+for THEME_DIR in "${THEME[@]}"; do
+	if [[ -r "$THEME_DIR" ]]; then
+		POWERLEVEL9K_TERM_SHELL_INTEGRATION=true; POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
+		source "$THEME_DIR/powerlevel10k.zsh-theme"
+		break
+	fi
+done
--- .config/zsh/core/30-functions.zsh
+++ .config/zsh/core/30-functions.zsh
@@ -1,12 +1,13 @@
 #
 # Functions
 #
+autoload -Uz add-zsh-hook
 
-if [ -f "${HOME}/.bash_functions" ]; then
+if [[ -f "${HOME}/.bash_functions" ]]; then
 	source "${HOME}/.bash_functions"
 fi
 
-if [ -f "${XDG_CONFIG_HOME:-${HOME}/.config}/bash/30-functions.bash" ]; then
+if [[ -f "${XDG_CONFIG_HOME:-${HOME}/.config}/bash/30-functions.bash" ]]; then
 	source "${XDG_CONFIG_HOME:-${HOME}/.config}/bash/30-functions.bash"
 fi
 
@@ -23,39 +24,19 @@ function append_env {
 	fi
 }
 
-# binding ctrl-s to sudo
-function sudo-command-line-s {
-	[[ -z $BUFFER ]] && zle up-history
-	local cmd="sudo "
-	if [[ ${BUFFER} == ${cmd}* ]]; then
-		CURSOR=$(( CURSOR-${#cmd} ))
-		BUFFER="${BUFFER#$cmd}"
-	else
-		BUFFER="${cmd}${BUFFER}"
-		CURSOR=$(( CURSOR+${#cmd} ))
-	fi
-	zle reset-prompt
-}
-zle     -N   sudo-command-line-s
-bindkey '^S' sudo-command-line-s
 
-# Change cursor shape for different vi modes.
-# Also fixes the cursor shape after exiting nvim.
-function zle-keymap-select {
-	if [[ $KEYMAP == vicmd ]] || [[ $1 = 'block' ]]; then
-		echo -ne '\e[1 q'
-	elif [[ $KEYMAP == main ]] || [[ $KEYMAP == viins ]] || [[ $KEYMAP = '' ]] || [[ $1 = 'beam' ]]; then
-		echo -ne '\e[5 q'
-	fi
-}
-zle -N zle-keymap-select
-# Start with beam shape cursor on zsh startup and after every command.
-zle-line-init() { zle-keymap-select 'beam'}
-
-_fix_cursor() {
-   echo -ne '\e[5 q'
-}
-precmd_functions+=(_fix_cursor)
+# cursor style
+if [[ -z $P9K_TTY || -z $ZVM_INIT_DONE || ! $ZLE_RPROMPT_INDENT ]]; then
+	zle-keymap-select () {
+		case $KEYMAP in
+			(vicmd | block) echo -ne '\e[1 q' ;;
+			(main | viins | '' | beam) echo -ne '\e[5 q' ;;
+		esac
+	}
+	zle-line-init() { zle-keymap-select 'beam' }
+	zle -N zle-keymap-select
+	zle -N zle-line-init
+fi
 
 function gpr {
 	local username=$(git config user.name)
@@ -88,17 +69,15 @@ function gpr {
 # https://gist.github.com/danydev/4ca4f5c523b19b17e9053dfa9feb246d
 # https://scarff.id.au/blog/2019/zsh-history-conditional-on-command-success/
 
-function __fd18et_prevent_write {
+__fd18et_prevent_write() {
 	__fd18et_LASTHIST=$1
 	return 2
 }
-function __fd18et_save_last_successed {
-	if [[ ($? == 0 || $? == 130) && -n $__fd18et_LASTHIST && -n $HISTFILE ]] ; then
+__fd18et_save_last_successed() {
+	if [[ ($? == 0 || $? == 130) && -n $__fd18et_LASTHIST && -n $HISTFILE ]]; then
 		print -sr -- ${=${__fd18et_LASTHIST%%'\n'}}
 	fi
 }
-
-autoload -U add-zsh-hook
 
 add-zsh-hook zshaddhistory __fd18et_prevent_write
 add-zsh-hook precmd __fd18et_save_last_successed
