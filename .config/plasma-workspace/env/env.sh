e2ce01c01732a07794cd06caf36e972cce11fc42
comsoi <40598925+comsoi@users.noreply.github.com>
2025-04-20 02:14:31 +0800

    update waybar and plasma env

--- .config/plasma-workspace/env/env.sh
+++ .config/plasma-workspace/env/env.sh
@@ -1,10 +1,4 @@
 # env.sh
-systemctl --user unset-environment QT_QPA_PLATFORMTHEME
-systemctl --user unset-environment QT_IM_MODULE
-systemctl --user daemon-reload
-unset QT_QPA_PLATFORMTHEME
-unset QT_IM_MODULE
-
 export QT_ENABLE_HIGHDPI_SCALING=1
 
 if [ -f /etc/systemd/system/display-manager.service ]; then
--- .config/plasma-workspace/env/reset.sh
+++ .config/plasma-workspace/env/reset.sh
@@ -1,17 +1,28 @@
 # reset.sh
-AUTO_THEME=$(qdbus org.freedesktop.portal.Desktop /org/freedesktop/portal/desktop org.freedesktop.portal.Settings.Read "org.freedesktop.appearance" "color-scheme" 2>/dev/null || echo "0")
 
-if [ "$AUTO_THEME" != "1" ] && [ "$AUTO_THEME" != "2" ]; then
-	_output=$(kreadconfig6 --group General --key ColorScheme)
-	case "$_output" in
-	*[Ll]ight*)
-		AUTO_THEME=2
-		;;
-	*[Dd]ark*)
-		AUTO_THEME=1
-		;;
-	esac
-fi
+unset QT_QPA_PLATFORMTHEME
+unset QT_IM_MODULE
+
+systemctl --user unset-environment QT_QPA_PLATFORMTHEME
+systemctl --user unset-environment QT_IM_MODULE
+
+systemctl --user daemon-reload
+
+(sleep 1 && systemctl --user restart xdg-desktop-portal.service) &
+
+_output=$(kreadconfig6 --group General --key ColorScheme)
+case "$_output" in
+*[Ll]ight*)
+	AUTO_THEME=2
+	;;
+*[Dd]ark*)
+	AUTO_THEME=1
+	;;
+*)
+	AUTO_THEME=2
+	;;
+esac
+unset _output
 
 if [ "$AUTO_THEME" = "1" ]; then
 	(sleep 6 && bash "$HOME/.local/share/flatpak/exports/share/dark-mode.d/10-kde.sh" --no-restart) &
