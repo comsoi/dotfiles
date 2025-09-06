0d4ed614e9f4edbf4159a69060300ea36c3f32b6
comsoi <40598925+comsoi@users.noreply.github.com>
2025-09-06 16:59:21 +0800

    feat: update autostart configuration, add brightness control module, and remove unused shaders

--- .config/waybar/toggle.sh
+++ .config/waybar/toggle.sh
@@ -2,8 +2,10 @@
 
 if systemctl --user is-active --quiet waybar.service; then
 	pkill xembedsniproxy
-	systemctl --user stop waybar.service
+	# systemctl --user stop waybar.service
+	kill -SIGUSR1 $(pidof waybar)
 else
-	systemctl --user start waybar.service
+	# systemctl --user start waybar.service
+	kill -SIGUSR1 $(pidof waybar)
 	exec uwsm app -- /usr/bin/xembedsniproxy
 fi
