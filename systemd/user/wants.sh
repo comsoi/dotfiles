6e2dea5efb7f52c183aa4fd631d9a2326407342d
comsoi <40598925+comsoi@users.noreply.github.com>
2025-10-27 22:50:58 +0800

    feat: [systemd] update

--- systemd/user/wants.sh
+++ systemd/user/wants.sh
@@ -1,6 +1,14 @@
 # KDE Plasma
 systemctl --user add-wants plasma-workspace-wayland.target plasma-workspace-wayland-env.service
 # Hyprland
-systemctl --user add-wants wayland-wm@hyprland.desktop.service hyprpolkitagent.service hypridle.service hyprpaper.service hyprsunset.service
+systemctl --user add-wants wayland-wm@hyprland.desktop.service hyprpolkitagent.service
+systemctl --user add-wants wayland-wm@hyprland.desktop.service hypridle.service
+systemctl --user add-wants wayland-wm@hyprland.desktop.service hyprpaper.service
+systemctl --user add-wants wayland-wm@hyprland.desktop.service hyprsunset.service
 # WM
-systemctl --user add-wants wayland-wm@.target waybar.service plasma-xembedsniproxy.service swaync.service sunsetr.service
+systemctl --user add-wants wayland-wm@.service swaync.service
+systemctl --user add-wants wayland-wm@.service waybar.service
+systemctl --user add-wants wayland-wm@.service plasma-xembedsniproxy.service
+systemctl --user add-wants wayland-wm@.service sunsetr.service
+elephant service enable
+systemctl --user add-wants wayland-wm@.service elephant.service
