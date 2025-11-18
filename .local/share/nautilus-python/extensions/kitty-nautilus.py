8ae1658622df454c7932612ec7f7fead6611d27b
comsoi <40598925+comsoi@users.noreply.github.com>
2025-11-18 22:06:07 +0800

    add: nautilus integration

--- .local/share/nautilus-python/extensions/kitty-nautilus.py
+++ .local/share/nautilus-python/extensions/kitty-nautilus.py
@@ -1,112 +1,98 @@
-#!/usr/bin/env python3
-"""
-Kitty Terminal Extension for Nautilus/Caja.
-支持通过右键菜单在多个目录或文件所在的目录中同时打开终端。
-"""
+# Adapted from wezterm: https://github.com/wez/wezterm/blob/main/assets/wezterm-nautilus.py
+# original copyright notice:
+#
+# Copyright (C) 2022 Sebastian Wiesner <sebastian@swsnr.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import subprocess
-from shutil import which
-import os
+import os.path
 
-try:
-    from urllib import unquote  # type: ignore
-    from urlparse import urlparse
-except ImportError:
-    from urllib.parse import unquote, urlparse
-
-from gi import get_required_version, require_version
-
-API_VERSION: str
-if (API_VERSION := get_required_version("Nautilus")) is not None:
-    try:
-        require_version("Gtk", "4.0")
-    except ValueError:
-        require_version("Gtk", "3.0")
-    from gi.repository import Nautilus as FileManager
-elif (API_VERSION := get_required_version("Caja")) is not None:
-    require_version("Gtk", "3.0")
-    from gi.repository import Caja as FileManager
-else:
-    raise RuntimeError("This module can only be executed as a Nautilus/Caja extension")
-
-from gi.repository import (
-    Gio,
-    GLib,
-    GObject,
-    Gtk,
-)  # noqa: E402 pylint: disable=wrong-import-position
+from gi import require_version
+from gi.repository import Nautilus, GObject, Gio, GLib
 
 
-class KittyTerminalExtension(GObject.GObject, FileManager.MenuProvider):
-    def _open_kitty(self, _menu, files):
-        """Open kitty terminal in the selected directories."""
-        fallback_paths = [
-            "kitty",
-            os.path.join(os.path.expanduser("~"), ".local/kitty.app/bin/kitty"),
-            os.path.join(os.path.expanduser("~"), ".local/bin/kitty"),
-            os.path.join(os.path.expanduser("~"), ".bin/kitty"),
-            os.path.join(os.path.expanduser("~"), "bin/kitty"),
-        ]
-        kitty_bin = None
-        for exe in fallback_paths:
-            # 如果在 PATH 中找到或可直接访问，则使用该可执行文件
-            if which(exe) or os.path.exists(exe):
-                kitty_bin = which(exe) or exe
-                break
+class OpenInKittyAction(GObject.GObject, Nautilus.MenuProvider):
+    def __init__(self):
+        super().__init__()
+        session = Gio.bus_get_sync(Gio.BusType.SESSION, None)
+        self._systemd = None
+        # Check if the this system runs under systemd, per sd_booted(3)
+        if os.path.isdir('/run/systemd/system/'):
+            self._systemd = Gio.DBusProxy.new_sync(session,
+                    Gio.DBusProxyFlags.NONE,
+                    None,
+                    "org.freedesktop.systemd1",
+                    "/org/freedesktop/systemd1",
+                    "org.freedesktop.systemd1.Manager", None)
 
-        if not kitty_bin:
-            return
+    def _open_terminal(self, path):
+        cmd = ['kitty', '--directory', path]
+        child = Gio.Subprocess.new(cmd, Gio.SubprocessFlags.NONE)
+        if self._systemd:
+            # Move new terminal into a dedicated systemd scope to make systemd
+            # track the terminal separately; in particular this makes systemd
+            # keep a separate CPU and memory account for Wezterm which in turn
+            # ensures that oomd doesn't take nautilus down if a process in
+            # wezterm consumes a lot of memory.
+            pid = int(child.get_identifier())
+            props = [("PIDs", GLib.Variant('au', [pid])),
+                ('CollectMode', GLib.Variant('s', 'inactive-or-failed'))]
+            name = 'app-nautilus-kitty-{}.scope'.format(pid)
+            args = GLib.Variant('(ssa(sv)a(sa(sv)))', (name, 'fail', props, []))
+            self._systemd.call_sync('StartTransientUnit', args,
+                    Gio.DBusCallFlags.NO_AUTO_START, 500, None)
 
+    def _menu_item_activated(self, _menu, paths):
+        for path in paths:
+            self._open_terminal(path)
+
+    def _make_item(self, name, paths):
+        item = Nautilus.MenuItem(name=name, label='Open in Kitty',
+            icon='kitty')
+        item.connect('activate', self._menu_item_activated, paths)
+        return item
+
+    def _paths_to_open(self, files):
+        paths = []
         for file in files:
-            if file.is_directory():
-                path = unquote(file.get_uri()[7:])
-                subprocess.Popen(
-                    [kitty_bin, "--directory", path]
-                )
+            location = file.get_location() if file.is_directory() else file.get_parent_location()
+            path = location.get_path()
+            if path and path not in paths:
+                paths.append(path)
+        if 10 < len(paths):
+            # Let's not open anything if the user selected a lot of directories,
+            # to avoid accidentally spamming their desktop with dozends of
+            # new windows or tabs.  Ten is a totally arbitrary limit :)
+            return []
+        else:
+            return paths
 
     def get_file_items(self, *args):
-        if len(args) == 1:
-            files = args[0]
-            window = None
+        # Nautilus 3.0 API passes args (window, files), 4.0 API just passes files
+        files = args[0] if len(args) == 1 else args[1]
+        paths = self._paths_to_open(files)
+        if paths:
+            return [self._make_item(name='KittyNautilus::open_in_kitty', paths=paths)]
         else:
-            window, files = args
-
-        directories = set()
-        for file_obj in files:
-            if file_obj.is_directory():
-                directories.add(file_obj.get_uri())
-            else:
-                parent_uri = file_obj.get_parent_uri()
-                if parent_uri:
-                    directories.add(parent_uri)
-
-        if not directories:
             return []
-
-        target_files = [FileManager.FileInfo.create_for_uri(uri) for uri in directories]
-
-        item = FileManager.MenuItem(
-            name="KittyTerminalExtension::OpenKitty",
-            label="Open in Kitty",
-            tip="Opens Kitty terminal in these directories",
-        )
-        item.connect("activate", self._open_kitty, list(target_files))
-        return [item]
 
     def get_background_items(self, *args):
-        if len(args) == 1:
-            current_folder = args[0]
-            window = None
+        # Nautilus 3.0 API passes args (window, file), 4.0 API just passes file
+        file = args[0] if len(args) == 1 else args[1]
+        paths = self._paths_to_open([file])
+        if paths:
+            return [self._make_item(name='KittyNautilus::open_folder_in_kitty', paths=paths)]
         else:
-            window, current_folder = args
-
-        if not current_folder.is_directory():
             return []
-
-        item = FileManager.MenuItem(
-            name="KittyTerminalExtension::OpenKittyBackground",
-            label="Open in Kitty",
-            tip="Opens Kitty terminal in this directory",
-        )
-        item.connect("activate", self._open_kitty, [current_folder])
-        return [item]
