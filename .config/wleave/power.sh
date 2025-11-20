124f69d651e5b9ba065270c8c13d7b3a8efdf5bd
comsoi <40598925+comsoi@users.noreply.github.com>
2025-11-20 18:48:53 +0800

    fix: wleave background position center css; hyprland wrong conf

--- .config/wleave/power.sh
+++ .config/wleave/power.sh
@@ -3,8 +3,16 @@
 if [[ "$1" == "exit" ]]; then
 	echo ":: Exit"
 	sleep 0.5
+	if [[ $WAYFIRE_SOCKET ]] && ! uwsm check is-active; then
+		killall wayfire
+		exit
+	fi
 	if [[ $NIRI_SOCKET ]]; then
 		niri msg action quit
+		exit
+	fi
+	if [[ $HYPRLAND_INSTANCE_SIGNATURE ]] && ! uwsm check is-active; then
+		hyprctl dispatch exit
 		exit
 	fi
 	uwsm stop
