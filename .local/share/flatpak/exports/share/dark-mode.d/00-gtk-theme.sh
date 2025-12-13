5628986680052a69848a88918eb53b4adf74f6db
comsoi <40598925+comsoi@users.noreply.github.com>
2025-12-13 20:02:29 +0800

    chore: disable color-scheme for gnome

--- .local/share/flatpak/exports/share/dark-mode.d/00-gtk-theme.sh
+++ .local/share/flatpak/exports/share/dark-mode.d/00-gtk-theme.sh
@@ -2,9 +2,15 @@
 
 GTK_THEME="Lavanda-Dark"
 ICON_THEME="WhiteSur-dark"
+USER_THEME="WhiteSur-Dark-solid"
 
 gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
 gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME"
-gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.shell.extensions.user-theme name "$USER_THEME"
 
 $HOME/.config/hypr/scripts/gtk.sh
+
+if ! systemctl --user --quiet is-active org.gnome.SettingsDaemon.Color.service || [[ "$XDG_CURRENT_DESKTOP" != "GNOME" ]]; then
+	gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+fi
+
--- .local/share/flatpak/exports/share/light-mode.d/00-gtk-theme.sh
+++ .local/share/flatpak/exports/share/light-mode.d/00-gtk-theme.sh
@@ -2,9 +2,16 @@
 
 GTK_THEME="Lavanda-Sea-Light"
 ICON_THEME="WhiteSur-light"
+USER_THEME="WhiteSur-Light-solid"
 
 gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
 gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME"
-gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
+gsettings set org.gnome.shell.extensions.user-theme name "$USER_THEME"
 
 $HOME/.config/hypr/scripts/gtk.sh
+
+# for gtk apps
+if ! systemctl --user --quiet is-active org.gnome.SettingsDaemon.Color.service || [[ "$XDG_CURRENT_DESKTOP" != "GNOME" ]]; then
+	gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
+fi
+
