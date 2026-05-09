3af2de01e86fdf2cbcba9d423ae0649b18b3dc65
comsoi <40598925+comsoi@users.noreply.github.com>
2026-05-09 20:53:20 +0800

    feat(hypr): update binds, decoration, layout; add show-desktop; remove hyprpaper

--- .config/hypr/scripts/gamemode.sh
+++ .config/hypr/scripts/gamemode.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-#   ____                                          _
-#  / ___| __ _ _ __ ___   ___ _ __ ___   ___   __| | ___
-# | |  _ / _` | '_ ` _ \ / _ \ '_ ` _ \ / _ \ / _` |/ _ \
-# | |_| | (_| | | | | | |  __/ | | | | | (_) | (_| |  __/
-#  \____|\__,_|_| |_| |_|\___|_| |_| |_|\___/ \__,_|\___|
-#
-#
 
-if [ -f $HOME/.local/state/hyprland/gamemode-enabled ]; then
-	hyprctl reload
-	rm $HOME/.local/state/hyprland/gamemode-enabled
-	notify-send "Gamemode deactivated" "Animations and blur enabled"
-else
-	hyprctl --batch "\
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+if [ "$HYPRGAMEMODE" = 1 ] ; then
+    hyprctl --batch "\
         keyword animations:enabled 0;\
+        keyword animation borderangle,0; \
         keyword decoration:shadow:enabled 0;\
         keyword decoration:blur:enabled 0;\
+	    keyword decoration:fullscreen_opacity 1;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
-	touch $HOME/.local/state/hyprland/gamemode-enabled
-	notify-send "Gamemode activated" "Animations and blur disabled"
+    hyprctl notify 1 5000 "rgb(40a02b)" "Gamemode [ON]"
+    exit
+else
+    hyprctl notify 1 5000 "rgb(d20f39)" "Gamemode [OFF]"
+    hyprctl reload
+    exit 0
 fi
+exit 1
