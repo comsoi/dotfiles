b59db8f652a206c603bb7f3275208f487a353296
comsoi <40598925+comsoi@users.noreply.github.com>
2026-05-09 21:29:50 +0800

    feat(wezterm): add color scheme

---
+++ .config/wezterm/style/color_scheme.lua
@@ -0,0 +1,23 @@
+local wezterm = require("wezterm")
+local M = {}
+
+local light_theme = nil
+local dark_theme = nil
+
+function M.scheme_for_appearance(appearance, config)
+	if appearance:find("Light") then
+		light_theme = "Catppuccin Frappe"
+		config.text_min_contrast_ratio = 1.25
+		return light_theme
+	end
+
+	dark_theme = "Catppuccin Mocha"
+	return dark_theme
+end
+
+function M.apply(config)
+	local appearance = wezterm.gui.get_appearance()
+	config.color_scheme = M.scheme_for_appearance(appearance, config)
+end
+
+return M
