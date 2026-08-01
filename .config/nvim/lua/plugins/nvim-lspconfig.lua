9a4870c287cd3eac3fb73bb41dbe49db463f6937
comsoi <40598925+comsoi@users.noreply.github.com>
2026-08-01 15:55:52 +0800

    chore: nvim

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,15 @@
 return {
 	{
 		"neovim/nvim-lspconfig",
-		opts = function()
-			local keys = require("lazyvim.plugins.lsp.keymaps").get()
-			-- disable a keymap
-			keys[#keys + 1] = { "<C-k>", false }
-		end,
+		opts = {
+			servers = {
+				['*'] = {
+					keys = {
+						-- Disable <C-k> keymap as in your previous config
+						{ "<C-k>", false },
+					},
+				},
+			},
+		},
 	},
 }
